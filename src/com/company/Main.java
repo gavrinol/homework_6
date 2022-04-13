0daaddf2199eee4b6321b5bdb3bff7dd1eb6e96d
gavrinol <102408582+gavrinol@users.noreply.github.com>
2022-04-13 20:15:52 +0300

    Сделанное

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Задание 1
+        Scanner Name = new Scanner(System.in);
+        System.out.println("Имя");
+        String firstName = Name.nextLine();
+        System.out.println("Отчество");
+        String middleName = Name.nextLine();
+        System.out.println("Фамилия");
+        String lastName = Name.nextLine();
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        // Задание 2
+        System.out.println(fullName.toUpperCase());
+
+        // Задание 3
+        fullName = fullName.replace('ё', 'е');
+        System.out.println(fullName);
+
+
+
     }
 }
+
